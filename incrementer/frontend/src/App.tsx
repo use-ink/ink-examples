82409d4053be50461fe47f8d535354a50aef0d9a
Sam Ruberti <samruberti@gmail.com>
2023-11-09 06:19:53 -0500

    feat: add incrementer ui (#26)
    * feat: add flipper ui example
    
    * feat: add incrementer ui
    
    ---------
    
    Co-authored-by: peetzweg/ <839848+peetzweg@users.noreply.github.com>
    Co-authored-by: peetzweg/ <phil.czek@gmail.com>

---
+++ incrementer/frontend/src/App.tsx
@@ -0,0 +1,60 @@
+import metadata from './assets/incrementer.json';
+import { CONTRACT_ROCOCO_ADDRESS } from './constants';
+import { useState } from 'react';
+import { Button, Card, ConnectButton, InkLayout, NumberInput } from 'ui';
+import { useCallSubscription, useContract, useTx, useWallet } from 'useink';
+import { useTxNotifications } from 'useink/notifications';
+import { pickDecoded, shouldDisable } from 'useink/utils';
+
+function App() {
+  const { account } = useWallet();
+  const contract = useContract(CONTRACT_ROCOCO_ADDRESS, metadata);
+  const getSub = useCallSubscription<number>(contract, 'get', [], {
+    defaultCaller: true,
+  });
+  const [incAmount, setIncAmount] = useState(1);
+  const inc = useTx(contract, 'inc');
+  useTxNotifications(inc);
+
+  return (
+    <InkLayout
+      className='md:py-12 md:p-6 p-4 h-screen flex items-center justify-center'
+      animationSrc='https://raw.githubusercontent.com/paritytech/ink-workshop/d819d10a35b2ac3d2bff4f77a96701a527b3ad3a/frontend/public/dark-sea-creatures.json'
+    >
+      <Card className='mx-auto p-6 flex flex-col w-full max-w-md backdrop-blur-sm bg-opacity-70'>
+        <h1 className='text-2xl font-bold'>
+          {metadata.contract.name.toUpperCase()}
+        </h1>
+
+        <p className='my-6'>
+          Current Number:{' '}
+          <b className='uppercase'>{pickDecoded(getSub.result)}</b>
+        </p>
+
+        <NumberInput
+          disabled={shouldDisable(inc)}
+          onChange={(v: number) => setIncAmount(v)}
+          value={incAmount}
+          min={1}
+          max={100}
+        />
+
+        {account ? (
+          <Button
+            disabled={shouldDisable(inc)}
+            onClick={() => inc.signAndSend([incAmount])}
+            className='mt-6'
+          >
+            {shouldDisable(inc)
+              ? 'Incrementing...'
+              : `Increment by ${incAmount}`}
+          </Button>
+        ) : (
+          <ConnectButton className='mt-6' />
+        )}
+      </Card>
+    </InkLayout>
+  );
+}
+
+export default App;
