dd7ec6edbfc41cdca76c24d90732578c6f1c35c7
peetzweg/ <phil.czek@gmail.com>
2023-11-10 14:31:53 +0100

    removes outdated duplicate code due to folder renaming

--- basic-contract-caller/frontend/src/App.tsx
+++ basic-contract-caller/frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import { Other } from './Other';
-import metadata from './assets/basic_contract_caller.json';
-import { BASIC_CONTRACT_ROCOCO_ADDRESS } from './constants';
 import { useEffect } from 'react';
 import { Button, Card, ConnectButton, InkLayout, formatContractName } from 'ui';
 import { useCall, useContract, useTx, useWallet } from 'useink';
 import { useTxNotifications } from 'useink/notifications';
 import { isPendingSignature, pickDecoded, shouldDisable } from 'useink/utils';
+import { Other } from './Other';
+import metadata from './assets/basic_contract_caller.json';
+import { BASIC_CONTRACT_ROCOCO_ADDRESS } from './constants';
 
 function App() {
   const { account } = useWallet();
@@ -23,12 +23,12 @@ function App() {
 
   return (
     <InkLayout
-      className='md:py-12 md:p-6 p-4 h-screen flex items-center justify-center'
-      animationSrc='https://raw.githubusercontent.com/paritytech/ink-workshop/d819d10a35b2ac3d2bff4f77a96701a527b3ad3a/frontend/public/dark-sea-creatures.json'
+      className="md:py-12 md:p-6 p-4 h-screen flex items-center justify-center"
+      animationSrc="https://raw.githubusercontent.com/paritytech/ink-workshop/d819d10a35b2ac3d2bff4f77a96701a527b3ad3a/frontend/public/dark-sea-creatures.json"
     >
-      <div className='flex flex-col md:flex-row md:justify-center justify-start items-stretch gap-3'>
-        <Card className='p-6 flex flex-col w-full max-w-md backdrop-blur-sm bg-opacity-70'>
-          <h1 className='text-2xl font-bold'>
+      <div className="flex flex-col justify-center items-center gap-3 h-full">
+        <Card className="p-6 flex flex-col w-full max-w-md backdrop-blur-sm bg-opacity-70">
+          <h1 className="text-2xl font-bold">
             {formatContractName(metadata.contract.name)}
           </h1>
 
@@ -36,7 +36,7 @@ function App() {
             <Button
               disabled={shouldDisable(flipAndGet)}
               onClick={() => flipAndGet.signAndSend()}
-              className='mt-6'
+              className="mt-6"
             >
               {isPendingSignature(flipAndGet)
                 ? 'Please sign transaction...'
@@ -45,7 +45,7 @@ function App() {
                 : 'Flip Other Contract'}
             </Button>
           ) : (
-            <ConnectButton className='mt-6' />
+            <ConnectButton className="mt-6" />
           )}
         </Card>
 
