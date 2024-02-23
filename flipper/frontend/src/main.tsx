683c3d28c3dacb56ca9ce3e9348b46ea38e61235
peetzweg/ <839848+peetzweg@users.noreply.github.com>
2024-02-23 11:33:19 +0100

    feat: `use-inkathon` flipper frontend example (#59)
    * replaces ink flipper example with standalone basic use-inkathon example
    
    * adds flipper frontend demo gif

--- flipper/frontend/src/main.tsx
+++ flipper/frontend/src/main.tsx
@@ -1,27 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'ui/style.css';
-import { UseInkProvider } from 'useink';
-import { RococoContractsTestnet } from 'useink/chains';
-import { NotificationsProvider } from 'useink/notifications';
-import App from './App.tsx';
-import './Global.css';
-import metadata from './assets/flipper.json';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UseInkProvider
-      config={{
-        dappName: metadata.contract.name,
-        chains: [RococoContractsTestnet],
-        caller: {
-          default: '5EyR7vEk7DtvEWeefGcXXMV6hKwB8Ex5uvjHufm466mbjJkR',
-        },
-      }}
-    >
-      <NotificationsProvider>
-        <App />
-      </NotificationsProvider>
-    </UseInkProvider>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
--- flipper/frontend/tailwind.config.js
+++ flipper/frontend/tailwind.config.js
@@ -1,2 +1,8 @@
-import config from '../../ui/tailwind.config';
-export default config;
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
